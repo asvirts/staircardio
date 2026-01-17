9a2ceb9158af2939b8819e39ab0e17b2be9ce5c7
Andrew Virts <andrew.virts@youversion.com>
2026-01-17 09:05:23 -0600

    Initial Commit

---
+++ staircardio/staircardioApp.swift
@@ -0,0 +1,32 @@
+//
+//  staircardioApp.swift
+//  staircardio
+//
+//  Created by Andrew Virts on 1/17/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct staircardioApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
